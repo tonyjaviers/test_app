f388039c6cd571fb8afa6b23235c38d95835a65c
Tony Sanchez <tsanchez@mrglasswindows.com>
2018-06-21 09:35:44 -0400

    fix created add flash

--- app/controllers/libros_controller.rb
+++ app/controllers/libros_controller.rb
@@ -5,10 +5,19 @@ class LibrosController < ApplicationController
   end
 
   def create
-    @libro=Libro.new(libro_params)
-    @libro.save
+   @libro=Libro.new(libro_params)
+  if @libro.save
+    flash[:notice] = "Libro fue creado"
+    redirect_to libro_path(@libro)
+  else
+    render 'new'
   end
 
+  def show
+    @libro=Libro.find(params[:id])
+  end
+
+  end
   private
      def libro_params
        params.require(:libro).permit(:title,:description)

fbad05bf5418cf3b7bf36fb0e78174eeb76a84c7
Tony Sanchez <tsanchez@mrglasswindows.com>
2018-06-21 11:50:06 -0400

    crear edit, update. show and index

--- app/controllers/libros_controller.rb
+++ app/controllers/libros_controller.rb
@@ -1,25 +1,44 @@
 class LibrosController < ApplicationController
 
+  def index
+    @libros=Libro.all
+  end
+
   def new
     @libro = Libro.new
   end
 
   def create
-   @libro=Libro.new(libro_params)
-  if @libro.save
-    flash[:notice] = "Libro fue creado"
-    redirect_to libro_path(@libro)
-  else
-    render 'new'
+    @libro=Libro.new(libro_params)
+    if @libro.save
+      flash[:notice] = "Libro fue creado"
+      redirect_to libro_path(@libro)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @libro=Libro.find(params[:id])
+    if @libro.update(libro_params)
+      flash[:notice] = "Libro was successfully updated"
+      redirect_to libro_path(@libro)
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @libro = Libro.find(params[:id])
   end
 
   def show
     @libro=Libro.find(params[:id])
   end
 
-  end
   private
-     def libro_params
-       params.require(:libro).permit(:title,:description)
-     end
+
+  def libro_params
+    params.require(:libro).permit(:title,:description)
+  end
 end
